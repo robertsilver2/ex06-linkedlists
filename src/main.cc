63c17a2ba3a99e5430ec9747f9ee78b0c2904651
robertsilver2 <robert_silver2@my.vcccd.edu>
2018-11-02 22:40:30 -0700

    henlo

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex06-LinkedList
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
+#include <iostream>
+#include "list.h"
+//using edu::vcccd::vc::csv15::list;
+//why not use this time? ^
+int main(int argc, char *argv[]) {
+    std::cout << "OwO oh henlo there world" <<std::endl;
+}

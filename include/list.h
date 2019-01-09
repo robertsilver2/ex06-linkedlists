7e37a1256d7539ca210d2016275dcc5243523554
robertsilver2 <robert_silver2@my.vcccd.edu>
2019-01-09 02:07:09 -0800

    henlo
    12-9-18:
    read up on templated classes, having a go at linked list
    -code line ~67 .h and 23 .cc causing a crash once destructor is reached. I think it is based on the "head-> next != NULL" line
    
    12-10-18:
    --getting a few things cleaned up, i.e. making copy constructor deep and putting in Null tests, added front back etc but not quite sure how i'm supposed to implement / call yet
    --figured basics of how to call front and back
    --got push front and push back going
    --turning in for the night
    --ending on the begin function, also not sure how to deallocate everything or use iterators
    
    --moving forward
    
    --12/13
      got dat erase working, also working on copy, just general implement
    
      --can't get constructor
    
    --1/9
    copy constructor implemented

--- include/list.h
+++ include/list.h
@@ -1,3 +1,15 @@
+/**
+ * CS V15 Data Structures and Algorithms
+ * CRN: 70342
+ * Assignment: ex06-LinkedList
+ *
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @robertsilver2
+ */
+
 #include <cstdlib>
 #include <utility>
 
